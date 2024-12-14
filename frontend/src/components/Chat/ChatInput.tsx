d1906e5937ea94db0b5d164e3fd60801f537a369
TC-X <92334384+TC-X@users.noreply.github.com>
2024-12-14 15:06:22 -0600

    feat:(frontend/chat-input): message submission foundation

--- frontend/src/components/Chat/ChatInput.tsx
+++ frontend/src/components/Chat/ChatInput.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { IconArrowUp } from '../../utils/icons'
 
 export default function ChatInput() {
+  const textareaRef = React.useRef<HTMLTextAreaElement>(null)
   const [userPrompt, setUserPrompt] = React.useState('')
 
-  // Handle input change and adjust textarea height
+  /* auto-grow textarea based on content */
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const { value } = e.currentTarget
     setUserPrompt(value)
@@ -13,15 +14,40 @@ export default function ChatInput() {
     e.currentTarget.style.height = `${e.currentTarget.scrollHeight}px` // Dynamically adjust height
   }
 
+  /* handle form submission */
+  const handleSubmit = (e?: React.FormEvent<HTMLFormElement>) => {
+    e && e.preventDefault()
+
+    // todo: send userPrompt to backend
+    console.log(userPrompt)
+
+    // clear textarea and focus
+    setUserPrompt('')
+    textareaRef.current?.focus()
+  }
+
+  /* handle keyboard shortcuts */
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (!e.shiftKey && e.key === 'Enter') {
+      e.preventDefault()
+      if (userPrompt.trim() === '') return // Prevent empty submission or multiple submissions
+      handleSubmit()
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className='p-2 flex justify-between bg-gray-100 dark:bg-neutral-700 gap-4 rounded-lg shadow-[0_0_24px_0_#00000004] cursor-text'>
         <textarea
           className='p-2 w-full max-h-[30dvh] bg-transparent border-none focus:outline-none placeholder:text-gray-400 dark:placeholder:text-neutral-400 resize-none'
           placeholder='Ask anything'
+          value={userPrompt}
+          ref={textareaRef}
           onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
           rows={1}
           dir='auto'
+          autoFocus
         />
         <button
           className='p-2 mt-auto hover:bg-white dark:hover:bg-neutral-600 rounded-md transition-[background-color_opacity] disabled:opacity-0'
