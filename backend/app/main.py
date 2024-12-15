4ee2212a257095e15193d35ff53ce8ba991374c1
TC-X <92334384+TC-X@users.noreply.github.com>
2024-12-15 02:38:56 -0600

    feat(backend): added random markdown responses

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,10 +9,10 @@ origins = ["http://localhost:3000"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend origin
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all HTTP methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # route routers
